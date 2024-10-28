dc8e604d03a41b4cfc40aced708083d30cd9a017
Maellxw922 <maxwellleon922@gmail.com>
2024-10-27 19:43:32 -0500

    Update rol.js

--- rol.js
+++ rol.js
@@ -1,8 +1,16 @@
 function UsuarioRegistrado(){
     var usuario1 = document.getElementById("usuario").value;
     var contra1 = document.getElementById("contra").value;
+    var usuario2 = document.getElementById("usuario").value;
+    var contra2 = document.getElementById("contra").value;
 
     if(usuario1=="Maxwell" && contra1 =="12345"){
+        document.location.href="rol.html";
+        alert("Bienvenido " + usuario1);
+    }else{
+        alert("Usuario y/o contraseña incorrectos")
+    }
+    if(usuario2=="Danna" && contra1 =="54321"){
         document.location.href="rol.html";
         alert("Bienvenido " + usuario1);
     }else{
